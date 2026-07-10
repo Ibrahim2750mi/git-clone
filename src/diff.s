e8d295abd67f14a457d3bee1018788ea481eac7b
Mohammad Ibrahim <cshore.2750mi@gmail.com>
2026-07-10 20:57:10 +0530

    check files only in current directory
    no sub directories till now

--- src/diff.s
+++ src/diff.s
@@ -2,6 +2,9 @@
 .include "modules/constants.s"
 .section .rodata
 
+currentdir:
+    .asciz "."
+
 filepath:
     .asciz ".gitignore"
 
@@ -14,18 +17,75 @@ new_string:
 modified_string:
     .asciz "modified: "
 
+buffer:
+    .space BUFFER_SIZE
+
 .section .text
+
 .global _start
 
 _start:
+    la a1, buffer
+    mv a2, BUFFER_SIZE
+
+
+
+.global recursedir
+
+recursedir:
+    # a0 = directory path
+    # a1 = buffer for file contents
+    # a2 = buffer size
+    # Returns: a0 = 0 on success, negative error code on failure
+
+    la s0, currentdir  # Save directory path
+    mv s1, a1  # Save buffer pointer
+    mv s2, a2  # Save buffer size
+
+    li a0, AT_FDCWD
+    mv a1, s0  # directory path
+    li a2, O_RDONLY | O_DIRECTORY
+    li a7, SYS_OPENAT
+    ecall   
+    bltz a0, recursedir_error
+    mv s3, a0  # Save the opened directory file descriptor
+
+    la a1, s1  # buffer for file contents
+    li a2, s2  # buffer size
+    li a7, SYS_GETDENTS64
+    ecall
+    bltz a0, recursedir_close_dir
+
+    mv s0, a1
+    recursedir_entry:
+        lhu s5, 16(s0)  # Read the d_reclen
+        lbu s6, 18(s0)  # Read the d_type
+
+        beq s6, DT_REG, recursedir_process_file
+    
+    l1:
+        add s0, s5, s0
+
+    recurseddir_process_file:
+        # Process the regular file
+        addi s4, s0, 19  # Move to the d_name field
+        call diff  # Call the diff function to compare the file
+        j l1
+        
+
+
+.global diff
+
+diff:
     # get file path
+    # s4 = filepath
     li t0, BUFFER_SIZE
     sub sp, sp, t0
     mv a0, sp
     la a1, parentdir
     call strcpy
 
-    la a1, filepath
+    mv a1, s4
     call strcat
 
     mv a1, a0
@@ -60,9 +120,7 @@ file_found:
     bnez a0, file_modified
 
 
-    li a0, 0
-    li a7, SYS_EXIT
-    ecall
+    ret
 file_not_found:
     # file does not exist, do something else
     li t0, BUFFER_SIZE
@@ -84,9 +142,7 @@ file_not_found:
     li t0, BUFFER_SIZE
     add sp, sp, t0
 
-    li a0, 0
-    li a7, SYS_EXIT
-    ecall
+    ret
     
 file_modified:
     # file exists and is modified, do something else
@@ -109,6 +165,4 @@ file_modified:
     li t0, BUFFER_SIZE
     add sp, sp, t0
 
-    li a0, 0
-    li a7, SYS_EXIT
-    ecall
+    ret
